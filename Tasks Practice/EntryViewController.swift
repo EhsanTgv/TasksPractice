b60334e4df97f9c7a37621544b7d5015c75e9ab6
EhsanTgv <EhsanTgv@gmail.com>
2022-04-30 17:54:05 +0300

    Implement TableView Outlet

---
+++ Tasks Practice/EntryViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Tasks Practice/TaskViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
